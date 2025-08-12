261ee466872cb40ec21852baf251c62bc654ad42
Szymon Szczukowski <szymon.szczukowski@polon-alfa.pl>
2025-08-12 13:05:19 +0200

    Add new file - periphery.

---
+++ Renesas_RA4M2_engine_TT/library/periphery.h
@@ -0,0 +1,18 @@
+/**
+ ******************************************************************************
+ *
+ * @file    periphery.h
+ *
+ * @brief   Periphery library.
+ *
+ * @author  Szymon Szczukowski
+ *
+ ******************************************************************************
+ */
+
+#ifndef PERIPHERY_H_
+#define PERIPHERY_H_
+
+
+
+#endif /* PERIPHERY_H_ */
